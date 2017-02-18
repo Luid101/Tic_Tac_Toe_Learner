8d6f46e3fd2a3c77a3f1d84945a8cbb588e30fe0
ArsalaBangash <arsalabangash@gmail.com>
2017-02-18 11:02:26 -0500

    Fixed AI Class

--- AI.py
+++ AI.py
@@ -1,12 +1,16 @@
 class AI:
 
     def __init__(self):
+        return
 
     def move(self):
+        return
 
     def has_lost(self):
+        return
 
     def has_won(self):
+        return
 
 
 
