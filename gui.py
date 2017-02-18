11345f348eaf44dd4a71ee73badd6e472a4afee6
Carl Marquez <carl.louis.marquez@gmail.com>
2017-02-18 17:00:48 -0500

    Finished 95% just need a scoreboard and other shizz the ai is dumb tho

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import pygame, sys
+import random
 
-class TicTacToeGui:
+class TicTacToe:
     SYMBOLS = ['x', 'o']
 
     def __init__(self):
@@ -11,6 +12,7 @@ class TicTacToeGui:
                             (0, 450): '-', (225, 450): '-', (450, 450): '-'}
         self._x_image = pygame.image.load("images/x.png")
         self._o_image = pygame.image.load("images/o.png")
+        self._game_display = pygame.display.set_mode((850, 650))
 
     def process_click(self, mouse_coords):
         """
@@ -80,10 +82,11 @@ class TicTacToeGui:
     def draw_board(self, game_display):
         # Draws the board
         game_display.fill((255, 255, 255))
-        pygame.draw.rect(game_display, (0, 0, 0), (200, 0, 25, 700), 0)
-        pygame.draw.rect(game_display, (0, 0, 0), (425, 0, 25, 700), 0)
-        pygame.draw.rect(game_display, (0, 0, 0), (0, 200, 700, 25), 0)
-        pygame.draw.rect(game_display, (0, 0, 0), (0, 425, 700, 25), 0)
+        pygame.draw.rect(game_display, (0, 0, 0), (200, 0, 25, 650), 0)
+        pygame.draw.rect(game_display, (0, 0, 0), (425, 0, 25, 650), 0)
+        pygame.draw.rect(game_display, (0, 0, 0), (0, 200, 650, 25), 0)
+        pygame.draw.rect(game_display, (0, 0, 0), (0, 425, 650, 25), 0)
+        pygame.draw.rect(game_display, (0, 0, 0), (650, 0, 10, 650), 0)
 
         # Draws the X's and O's
         for coords in self._game_array:
@@ -92,24 +95,74 @@ class TicTacToeGui:
             elif self._game_array[coords] == 'o':
                 game_display.blit(self._o_image, coords)
 
+    def win(self, symbol):
+        """
+        Determines whether player symbol has won
+        :param symbol: 'x' or 'o'
+        :return: True if player has won, False otherwise
+        """
+        a = self._game_array  # More convenient name
+        if all(x == symbol for x in [a[(0, 0)], a[(0, 225)], a[(0, 450)]]):
+            return True
+        elif all(x == symbol for x in [a[(225, 0)], a[(225, 225)],
+                                       a[(225, 450)]]):
+            return True
+        elif all(x == symbol for x in [a[(450, 0)], a[(450, 225)],
+                                       a[(450, 450)]]):
+            return True
+        elif all(x == symbol for x in [a[(0, 0)], a[(225, 0)],
+                                       a[(450, 0)]]):
+            return True
+        elif all(x == symbol for x in [a[(0, 225)], a[(225, 225)],
+                                           a[(450, 225)]]):
+            return True
+        elif all(x == symbol for x in [a[(0, 450)], a[(225, 450)],
+                                       a[(450, 450)]]):
+            return True
+        elif all(x == symbol for x in [a[(0, 0)], a[(225, 225)],
+                                       a[(450, 450)]]):
+            return True
+        elif all(x == symbol for x in [a[(0, 450)], a[(225, 225)],
+                                       a[(450, 0)]]):
+            return True
+        else:
+            return False
+
+    def ai_input(self):
+        positions = list(self._game_array.keys())
+        found = False
+        if self._turn % 2 == 1 and self._turn < 9:
+            while not found:
+                random_pos = random.choice(positions)
+                if self._game_array[random_pos] == '-':
+                    found = True
+                    self._game_array[random_pos] = self.SYMBOLS[self._turn % 2]
+                    self._turn += 1
+
     def run(self):
         # Sets up GUI
-        game_display = pygame.display.set_mode((650, 650))
         pygame.display.set_caption("Thicc Tac Toe")
         clock = pygame.time.Clock()
+        won = False
 
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit(0)
-                elif event.type == pygame.MOUSEBUTTONDOWN:
+                elif event.type == pygame.MOUSEBUTTONDOWN and not won \
+                        and self._turn < 9 and self._turn % 2 == 0:
                     self.process_click(pygame.mouse.get_pos())
+                elif not won and self._turn < 9 and self._turn % 2 == 1:
+                    self.ai_input()
 
-            self.draw_board(game_display)
+            self.draw_board(self._game_display)
             pygame.display.update()
+            x_win = self.win('x')
+            o_win = self.win('o')
+            won = x_win or o_win
             clock.tick(60)
 
 if __name__ == "__main__":
-    gui = TicTacToeGui()
-    gui.run()
+    game = TicTacToe()
+    game.run()
